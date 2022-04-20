5e7f7ce3a1acdf102d3a7d731e850296ea8acf59
Mayur Wagh <69896733+Mayurwagh98@users.noreply.github.com>
2022-04-20 17:23:08 +0530

    Add files via upload

---
+++ Day1 jsx-challenge/src/App.jsx
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+      <>
+        <h1>Netflix Series</h1>
+        <p>Here is the list of top 5 netflix series</p>
+        <ol>
+          <li>Dark</li>
+          <li>Money Heist</li>
+          <li>Mr Robot</li>
+          <li>My Holo Love</li>
+          <li>My First 2 Love</li>
+        </ol>
+      </>
+
+    </div>
+  );
+}
+
+export default App;
