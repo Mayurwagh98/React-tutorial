9c4a5fff40e397d6e56ed0f78f7fa0ef92c9b141
Mayurwagh98 <mswagh1998@gmail.com>
2022-04-22 17:02:02 +0530

    expressions in react

---
+++ Day 2 (expressions-react)/src/App.jsx
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  const name = "Mayur"
+
+  return (
+    <div className="App">
+      <>
+        <h1>My name is {name}</h1>
+        <p>My lucky number is {5 + 6}</p>
+      </>
+    </div>
+  );
+}
+
+export default App;
