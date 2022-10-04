35f8b181e5ef15b1e008424f7915babb183e4514
Mayur Wagh <69896733+Mayurwagh98@users.noreply.github.com>
2022-10-04 17:56:31 +0530

    Update App.js

--- Day8 (simple calculations using import export)/src/App.js
+++ Day8 (simple calculations using import export)/src/App.js
@@ -4,13 +4,13 @@ export default function App() {
   return (
     <div className="App">
       <ul>
-        <li>addition of two numbers are {add(2, 3)}</li>
+        <li>Addition of two numbers are {add(2, 3)}</li>
 
-        <li>addition of two numbers are {sub(2, 3)}</li>
+        <li>Subtraction of two numbers are {sub(2, 3)}</li>
 
-        <li>addition of two numbers are {multi(2, 3)}</li>
+        <li>Multiplication of two numbers are {multi(2, 3)}</li>
 
-        <li>addition of two numbers are {div(2, 3)}</li>
+        <li>Division of two numbers are {div(2, 3)}</li>
       </ul>
     </div>
   );
